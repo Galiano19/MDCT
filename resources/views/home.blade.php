74b067dc5502ccbe45946e87fd0c8afc43e68360
Gali <galihorizon21@gmail.com>
2019-05-15 20:01:13 +0200

    Auto stash before merge of "master" and "MDCT/master"
    frontend update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.lobby')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">MDCT
+                
+                </div>
+                <p class="fancy nickname"><span>{{Auth::user()->nick}}</span></p>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in as {{Auth::user()->nick}}!<br>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm">
+                                <div class="hexagondiv">
+                                    
+                                </div>
+                            </div>
+                            <div class="col-sm">
+                            </div>
+                        </div>
+                    </div>
                     <a href="{{ action('CharacterController@indexCharacters') }}">Characters</a><br>
                     <a href="{{ url('/campaigns/') }}">Campaigns</a>
                 </div>
