b5daf5d05adf56ec3f849b4179f491a4613e77f7
Gali <galihorizon21@gmail.com>
2019-05-21 20:54:09 +0200

    frontednd characters

--- resources/views/characters/showall.blade.php
+++ resources/views/characters/showall.blade.php
@@ -1,22 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.lobby')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="md-12">
-            <div class="col-sm-2">Class</div>
-            <div class="col-sm-5">Name</div>
-            <div class="col-sm-2">Level</div>
-            <div class="col-sm-3">Modify</div>
-        </div>
+    <div class="row justify-content-center">
+        <div class="panel-heading text-justify">CHARACTERS</div>
+        <p class="fancy nickname"><span>{{Auth::user()->nick}}</span></p>
+    </div>
+</div>
+<div class="container">
+    <div class="d-flex flex-wrap justify-content-around " style="margin-top:5%;">
+
         @foreach ($characters as $character)
-            <div class="md-12">
-                <div class="col-sm-2">{{ $character->class }}</div>
-                <div class="col-sm-5">{{ $character->name }}</div>
-                <div class="col-sm-2">{{ $character->level }}</div>
-                <div class="col-sm-3"><a href="{{ action('CharacterController@modifyCharacters',['charid' => $character->id]) }}">Modify</a><br></div>
+        <div class="card charactercard" style="width: 18rem;">
+            <div class="card-body ">
+                <h5 class="card-title">{{ $character->name }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $character->class }}</h6>
+                <div class="row justify-content-around">
+                    <p class="card-text">LEVEL: {{ $character->level }}</p>
+                    <a href="{{ action('CharacterController@modifyCharacters',['charid' => $character->id]) }}" class="card-link">MODIFY</a>
+                </div>            
             </div>
+        </div>
         @endforeach
+    </div>
+</div>
+<div class="container">
+    <div class="row align-items-stretch justify-content-end" style="margin-bottom:2em;">
+        <a  class="btn btn-dark btn-lg buttonselect" href="{{ action('CharacterController@indexCharacters') }}" role="button">
+            BACK
+        </a>
     </div>
 </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">MDCT</div>
-                <p class="fancy nickname"><span>{{Auth::user()->nick}}</span></p>
-                </div>
-            </div>
+    <div class="row justify-content-center">
+        <div class="panel-heading text-justify">MDCT</div>
+        <p class="fancy nickname"><span>{{Auth::user()->nick}}</span></p>
+    </div>
+</div>
+<div class="container">
+    <div class="row selectorrow">
+        <div class="col-md-6 selectbtn" align="center">
+            <a href="{{ action('CharacterController@indexCharacters') }}">
+                <img src="{{ asset('images/home/charactersbtn.jpg') }}" alt="">
+            </a>                       
         </div>
-        <div class="row selectorrow">
-            <div class="col-md-6 selectbtn" align="center">
-                <a href="{{ action('CharacterController@indexCharacters') }}">
-                    <img src="{{ asset('images/home/charactersbtn.jpg') }}" alt="">
-                </a>                       
-            </div>
-            <div class="col-md-6 selectbtn" align="center">
-                <a href="{{ url('/campaigns/') }}"><img src="{{ asset('images/home/campaignsbtn.jpg') }}" alt=""></a>
-            </div>
-        </div>
-                
+        <div class="col-md-6 selectbtn" align="center">
+            <a href="{{ url('/campaigns/') }}"><img src="{{ asset('images/home/campaignsbtn.jpg') }}" alt=""></a>
         </div>
     </div>
 </div>
