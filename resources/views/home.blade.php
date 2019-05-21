23baeae17c09a663e71a050188ea13cf78717b6e
Gali <galihorizon21@gmail.com>
2019-05-21 16:29:01 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,28 +7,6 @@
             <div class="panel panel-default">
                 <div class="panel-heading">MDCT</div>
                 <p class="fancy nickname"><span>{{Auth::user()->nick}}</span></p>
-<<<<<<< HEAD
-=======
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm">
-                                <div class="hexagondiv">
-                                    
-                                </div>
-                            </div>
-                            <div class="col-sm">
-                            </div>
-                        </div>
-                    </div>
-                    <a href="{{ action('CharacterController@indexCharacters') }}">Characters</a><br>
-                    <a href="{{ action('CampaignController@indexCampaigns') }}">Campaigns</a>
->>>>>>> master
                 </div>
             </div>
         </div>
@@ -39,7 +17,7 @@
                 </a>                       
             </div>
             <div class="col-md-6 selectbtn" align="center">
-                <a href="{{ url('/campaigns/') }}"><img src="{{ asset('images/home/campaignsbtn.jpg') }}" alt=""></a>
+                <a href="{{ action('CampaignController@indexCampaigns') }}"><img src="{{ asset('images/home/campaignsbtn.jpg') }}" alt=""></a>
             </div>
         </div>
                 
